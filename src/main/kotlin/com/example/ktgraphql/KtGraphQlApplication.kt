e9d8cc6614849966a6b583c67a1588f3a25e429a
kakaoent-leo <leo.ee@kakaoent.com>
2023-12-10 15:51:14 +0900

    first commit

---
+++ src/main/kotlin/com/example/ktgraphql/KtGraphQlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtGraphQlApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtGraphQlApplication>(*args)
+}
